4c906d28d35e8780e35c095ed9a3b5e4dac444c8
Amanjot Singh <amanjot02121202021@msijanakpuri.com>
2023-02-21 19:18:14 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,18 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
         appBar: AppBar(
-          title: Text("My First APP!!!"),
+          title: Center(child: Text("Liquid Galaxy App")),
+          backgroundColor: Colors.black,
+        ),
+        body: const Padding(
+          padding: EdgeInsets.fromLTRB(50, 180, 50, 60),
+          child: Image(
+            image: AssetImage('images/LIQUIDGALAXYLOGO.png'),
+            height: 200,
+            width: 500,
+          ),
         ),
       ),
     ),
